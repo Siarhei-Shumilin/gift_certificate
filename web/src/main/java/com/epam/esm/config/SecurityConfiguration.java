6dad70c8e93ea450ff527d640f71a420ebb0dcf4
Siarhei Shumilin <siarhei_shumilin@epam.com>
2020-01-31 14:47:16 +0300

    forbidded to delete certificate for user and guest

--- web/src/main/java/com/epam/esm/config/SecurityConfiguration.java
+++ web/src/main/java/com/epam/esm/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.epam.esm.config;
 import com.epam.esm.config.filter.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,7 +31,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/certificates/authenticate").permitAll()
+                .authorizeRequests()
+                .antMatchers("/certificates").permitAll()
+                .antMatchers("/certificates/authenticate").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/certificates/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/tags").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().and().sessionManagement()
