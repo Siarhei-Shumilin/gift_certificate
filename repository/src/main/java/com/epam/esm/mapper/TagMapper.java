24c6227d38c7076e3460691969c089c10d218a71
Siarhei Shumilin <siarhei_shumilin@epam.com>
2020-03-01 20:17:40 +0300

    changed query for the most popular tag

--- repository/src/main/java/com/epam/esm/mapper/TagMapper.java
+++ repository/src/main/java/com/epam/esm/mapper/TagMapper.java
@@ -32,7 +32,9 @@ public interface TagMapper {
     @Select("SELECT tags.id, tags.name FROM tags INNER JOIN connecting ON tags.id=connecting.tag_id"
             + " WHERE connecting.certificate_id = " +
             "(SELECT certificate_id FROM purchases WHERE user_id = " +
-            "(SELECT user_id FROM purchases where price =(select MAX(price) from purchases limit 1) ORDER BY price DESC LIMIT 1) GROUP BY certificate_id ORDER BY COUNT(certificate_id) DESC LIMIT 1) GROUP BY tag_id ORDER BY COUNT(tag_id) DESC LIMIT 1")
+            "(SELECT user_id FROM purchases GROUP BY user_id ORDER BY sum(price) DESC LIMIT 1)" +
+            " GROUP BY certificate_id ORDER BY COUNT(certificate_id) DESC LIMIT 1) " +
+            "GROUP BY tag_id ORDER BY COUNT(tag_id) DESC LIMIT 1")
     Tag findMostPopularTag();
 
     @Select({"<script>",
